6fcf48d9cf9314e2331fd5a3dda7269e2d247653
Leo Durrant <poweredbykurisu@hotmail.com>
2022-12-24 13:41:43 +0000

    wip: made callbacks more vue like

--- src/components/SectionButtons.vue
+++ src/components/SectionButtons.vue
@@ -2,20 +2,28 @@
 import { getImageUrl } from "@/util/utils.js";
 
 const props = defineProps<{
-	src: string
-	text: string,
-	onclick: () => void
-}>()
+  src: string;
+  text: string;
+}>();
 
+const emit = defineEmits<{
+  (e: "whenClick"): void;
+}>();
+
+function test() {
+  emit("whenClick");
+}
 </script>
 
 <template>
-	<div class="shadow shadow-grey bg-red-700 text-white p-2 m-2 select-none cursor-pointer hover:bg-red-800">
-		<button class="flex align-middle items-center gap-5">
-			<img :alt="props.text" :src="getImageUrl(props.src)" /> 
-			<div> 
-				{{props.text}} 
-			</div>
-		</button>
-	</div>
+  <div
+    class="shadow shadow-grey bg-red-700 text-white p-2 m-2 select-none cursor-pointer hover:bg-red-800"
+  >
+    <button class="flex align-middle items-center gap-5" @click="test()">
+      <img :alt="props.text" :src="getImageUrl(props.src)" />
+      <div>
+        {{ props.text }}
+      </div>
+    </button>
+  </div>
 </template>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import events from '@/content/events.json';
+import events from "@/content/events.json";
 
-const route = useRoute()
+const route = useRoute();
 
 useHead({
   bodyAttrs: {
@@ -9,15 +9,15 @@ useHead({
   },
   meta: [
     {
-      property: 'og:title',
+      property: "og:title",
       content: route.meta.title,
-    }
+    },
   ],
-})
+});
 
 const BUILD_DATE = import.meta.env.VITE_APP_BUILD_EPOCH
   ? new Date(Number(import.meta.env.VITE_APP_BUILD_EPOCH))
-  : undefined
+  : undefined;
 
 let activeEvents = events.events;
 
@@ -29,6 +29,10 @@ function dateFromStr(str: string | undefined): Date {
   }
 }
 
+// TODO: Replace this!
+function placeholder() {
+  console.log("Hello!");
+}
 </script>
 
 <template>
@@ -39,33 +43,51 @@ function dateFromStr(str: string | undefined): Date {
     <div
       class="p-2 container relative max-w-2xl mx-auto bg-gray-700 backdrop-blur-md bg-blend-normal shadow-xl shadow-slate-700/10 ring-1 ring-gray-900/5"
     >
-      <header class="px-4 pt-6 prose-sm md:px-6 md:prose flex-col items-center text-center justify-center m-auto w-full flex">
+      <header
+        class="px-4 pt-6 prose-sm md:px-6 md:prose flex-col items-center text-center justify-center m-auto w-full flex"
+      >
         <img src="@/assets/logo.svg" class="w-96" alt="compsoc" />
-        <div class="text-white text-xl"> Royal Holloway Computing Society </div>
+        <div class="text-white text-xl">Royal Holloway Computing Society</div>
       </header>
       <main>
-
         <div class="grid md:grid-cols-2">
-          <SectionButtons text="Membership" src="membership.svg" onclick="() => {console.log('hi')}"/>
-          <SectionButtons text="Committee" src="committee.svg" onclick="() => {console.log('hi')}"/>
-          <SectionButtons text="Student Union" src="su.svg" onclick="() => {console.log('hi')}"/>
-          <SectionButtons text="Events" src="events.svg" onclick="() => {console.log('hi')}"/>
+          <SectionButtons
+            text="Membership"
+            src="membership.svg"
+            @whenClick="placeholder"
+          />
+          <SectionButtons
+            text="Committee"
+            src="committee.svg"
+            @whenClick="placeholder"
+          />
+          <SectionButtons
+            text="Student Union"
+            src="su.svg"
+            @whenClick="placeholder"
+          />
+          <SectionButtons
+            text="Events"
+            src="events.svg"
+            @whenClick="placeholder"
+          />
         </div>
 
-        <div class="font-semibold text-2xl text-white px-5"> Upcoming Events </div>
+        <div class="font-semibold text-2xl text-white px-5">
+          Upcoming Events
+        </div>
 
         <div class="flex-col">
           <Event
-            v-for="item in activeEvents" 
+            v-for="item in activeEvents"
             :key="item.event_title"
-            :title=item.event_title
-            :desc=item.desc
+            :title="item.event_title"
+            :desc="item.desc"
             :datestart="dateFromStr(item.date_start)"
             :dateend="dateFromStr(item.date_end)"
             :links="item.links"
           />
         </div>
-        
       </main>
       <footer class="py-6 text-xs text-center font-sm text-gray-700">
         website by compsoc
@@ -73,7 +95,7 @@ function dateFromStr(str: string | undefined): Date {
     </div>
   </div>
   <!-- Here I'll put the canvas / whatever will take up the background -->
-  <div class="w-full h-full fixed top-0 left-0 -z-10 bg-gradient-to-bl from-[#eb6420] to-[#ff7e42] overflow-hidden">
-    
-  </div>
+  <div
+    class="w-full h-full fixed top-0 left-0 -z-10 bg-gradient-to-bl from-[#eb6420] to-[#ff7e42] overflow-hidden"
+  ></div>
 </template>
