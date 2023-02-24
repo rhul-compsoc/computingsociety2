b83fcdcbed2934aca19f7c0bd52c8193d04c1b6b
Leo Durrant <poweredbykurisu@hotmail.com>
2023-02-24 16:57:59 +0000

    fix: alt text, portrait sizing, and card spacing

--- src/components/CommitteeMember.vue
+++ src/components/CommitteeMember.vue
@@ -36,6 +36,12 @@
                 default(filename: String): string {
                     return "img/committee-portraits/" + filename;
                 }
+            },
+            altPortraitText: {
+                type: Function,
+                default(name: String) : string {
+                    return name + "'s portrait";
+                }
             }
         }
     }
@@ -44,13 +50,15 @@
 
 <template>
 
-    <div class="bg-white w-40 m-3 p-5 shadow-xl shadow-grey flex-col justify-center text-center">
-        <img :src="getImagePathMethod(portrait ?? 'placeholder.png')" alt="{{name}}'s portrait" />
-        <div class="text-xl">{{ name }}</div>
-        <div class="m-3">{{ role }}</div>
-        <div class="flex flex-row space-x-4 justify-center">
-            <div><a href={github}> <img src="@/assets/github.svg" class="w-5" alt="github" /> </a></div>
-            <div><a href={linkedin}> <img src="@/assets/linkedin.svg" class="w-5" alt="linkedin" /> </a></div>
+    <div class="bg-white w-40 m-3 p-5 shadow-xl shadow-grey flex-col justify-center text-center ">
+        <div class="flex flex-col justify-evenly h-full">
+            <img class="w-32 h-32 mx-auto object-cover" :src="getImagePathMethod(portrait ?? 'placeholder.png')" :alt="altPortraitText(name)" />
+            <div class="text-xl">{{ name }}</div>
+            <div class="m-3">{{ role }}</div>
+            <div class="flex flex-row space-x-4 justify-center">
+                <div><a href={github}> <img src="@/assets/github.svg" class="w-5" alt="github" /> </a></div>
+                <div><a href={linkedin}> <img src="@/assets/linkedin.svg" class="w-5" alt="linkedin" /> </a></div>
+            </div>
         </div>
     </div>
 
