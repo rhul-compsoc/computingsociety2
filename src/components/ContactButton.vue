239fe662ab49914fcfd45362949c01b42c9e2608
Stephanie Flower <stephanie.flower.15@gmail.com>
2023-09-21 20:23:11 +0100

    setup icons for contact us

--- src/components/ContactButton.vue
+++ src/components/ContactButton.vue
@@ -3,8 +3,26 @@
     export default {
         props: {
             icon: {
-                
-            }
+                type: String,
+                required: false,
+                default: undefined,
+            },
+            title: {
+                type: String,
+                required: true,
+                default: "_ Title",
+            },
+            link: {
+                type: String,
+                required: true,
+                default: "/",
+            },
+            getImagePathMethod: {
+                type: Function,
+                default(filename: String): string {
+                    return "img/contact-icons/" + filename;
+                }
+            },
         }
     }
 
@@ -13,7 +31,10 @@
 <template>
 
     <div>
-
+        <a :href="link">
+            <img :src="getImagePathMethod(icon)" :alt="title" />
+            <div> {{ title }} </div>
+        </a>
     </div>
 
 </template>
